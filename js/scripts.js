c83c304de6964094e13f19a6ea9ed700a9760de4
Matt Botz and J. Ryan Conklin <botzmatt@yahoo.com>
2016-05-23 21:08:03 -0700

    add variables and spans to showing results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
+  $("#blanks form").submit(function(event) {
 
+    var userInput = $("input#user").val();
+    var foodInput = $("input#food").val();
+    var animalInput = $("input#animal").val();
+    var bandInput = $("input#band").val();
+    var hairColorInput = $("input#hairColor").val();
+    var jobInput = $("input#job").val();
 
+    $(".user").append(userInput);
+    $(".food").append(foodInput);
+    $(".animal").append(animalInput);
+    $(".band").append(bandInput);
+    $(".hairColor").append(hairColorInput);
+    $(".job").append(jobInput);
 
-  
+    $("#results").show();
+    event.preventDefault();
+  });
 });
